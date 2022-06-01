fbb83dd489dfa811bde887e5f40d960651edb2da
Brady Bott <86979515+DaTrueBrad@users.noreply.github.com>
2022-06-01 14:03:40 -0600

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,11 +2,23 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const Details = () => {
-  const {id} = useParams()
+  const [name, setName] = useState("")
+
+  const {pokemon} = useParams()
+
+  useEffect(() => {
+    axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((res) => {
+      setName(res.data.name)
+    })
+  }, [])
+  
+  
   return (
     <div>
       <h1>Details Page</h1>
-      <h2>{id}</h2>
+      <h2>{name}</h2>
       <h3>This page is dynamic, and can show information based off of parameters in the URL!</h3>
 
     </div>
