02f7a8f9bc9632836a0173510b2b522c78c7e8e1
Brady Bott <bottbrady@gmail.com>
2022-06-08 08:18:01 -0600

    finished week 3 demo

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import '../App.css'
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import axios from "axios";
 
 const Item = ({ task, id, list, setList }) => {
+  console.log(task);
+  const [checked, setChecked] = useState(false);
 
-  const [checked, setChecked] = useState(false)
-
-  const handleDelete = (id) => {
-    list.splice(id, 1)
-    setList([...list])
-  }
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:4000/api/deleteTask/${task.task_id}`)
+      .then((res) => {
+        list.splice(id, 1);
+        setList([...list]);
+      });
+  };
 
   return (
-    <div id='item'>
-        <h2 onClick={() => setChecked(!checked)} className={checked ? 'checked' : null}>{task.task}</h2>
-        <h4>{task.category}</h4>
-        <h1 onClick={() => handleDelete(id)}>x</h1>
+    <div id="item">
+      <h2
+        onClick={() => setChecked(!checked)}
+        className={checked ? "checked" : null}
+      >
+        {task.task_name}
+      </h2>
+      <h4>{task.title}</h4>
+      <h1 onClick={handleDelete}>x</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,70 @@
-import React, { useState } from 'react'
-import ListDisplay from './ListDisplay'
-import '../App.css'
+import React, { useEffect, useState } from "react";
+import ListDisplay from "./ListDisplay";
+import { useFormik } from "formik";
+import "../App.css";
+import axios from "axios";
 
 const Main = () => {
+  const [allCategories, setAllCategories] = useState([]);
+  const [list, setList] = useState([]);
 
-  const [task, setTask] = useState('')
-  const [category, setCategory] = useState('')
-  // const [item, setItem] = useState({})
-  const [list, setList] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/api/getCategories')
+      .then((res) => setAllCategories(res.data))
+    axios
+      .get('http://localhost:4000/api/getAllTasks')
+      .then((res) => setList(res.data))
+  },[])
 
-  const handleChange = (e) => {
-    setTask(e.target.value)
-  }
+  const catOptions = allCategories.map((cat, index) => {
+    return <option value={cat.category_id}>{cat.title}</option>
+  })
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setList([...list, {task: task, category: category}])
-    setTask('')
-  }
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      category: null,
+    },
+    onSubmit: (values) => {
+      axios
+        .post('http://localhost:4000/api/addTask', values)
+        .then((res) => setList(res.data))
+    },
+  });
 
   return (
-    <div id='main'>
+    <div id="main">
       <h1>Add Task</h1>
-        <form onSubmit={(e) => handleSubmit(e)} id='item-form'>
-          <div id='input'>
-            <input type='text'placeholder='task' onChange={(e) => handleChange(e)} value={task}></input>
-            <select onChange={(e) => setCategory(e.target.value)}>
-                <option defaultValue disabled selected>category</option>
-                <option value='chores'>chores</option>
-                <option value='errands'>errands</option>
-                <option value='work'>work</option>
-            </select>
-          </div>
-            <button type='submit'>Add</button>
-          </form>
-        <ListDisplay list={list} setList={setList}/>
-      </div>
-  )
-}
+      <form onSubmit={formik.handleSubmit} id="item-form">
+        <div id="input">
+          <input
+            name="name"
+            type="text"
+            placeholder="task"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          ></input>
+          <input
+            name="description"
+            type="text"
+            placeholder="Describe your Task"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          ></input>
+          <select name="category"value={formik.values.category} onChange={formik.handleChange}>
+            <option defaultValue disabled selected>
+              category
+            </option>
+            {catOptions}
+          </select>
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      <ListDisplay list={list} setList={setList} />
+    </div>
+  );
+};
 
-export default Main
+export default Main;
